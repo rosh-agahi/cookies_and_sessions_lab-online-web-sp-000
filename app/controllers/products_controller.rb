9796cca7401315c5a0db96d219a3d5cdc93bbb10
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-10 23:08:45 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,17 @@
-class ProductsController < ActionController::Base
+
+class ProductsController < ApplicationController
+    def index
+        @cart = cart
+    end
+
+    def add
+        cart << product_params
+        redirect_to :products
+    end
   
-  def index 
-  end
-  
-  def add
-      cart << params[:product]
-      redirect_to root_path
-  end
-  
-end 
+  private
+    
+    def product_params
+        params.require(:product)
+    end
+end
