5fe41b51a158726929811fddcb8294fc8625a1f4
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-10 23:15:30 +0000

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,17 @@
+
+class ProductsController < ApplicationController
+    def index 
+        @cart = cart
+    end
+
+    def add
+        cart << product_params
+        redirect_to root_path
+    end
+  
+  private
+    
+    def product_params
+        params.require(:product)
+    end
+end
